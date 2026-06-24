d4bcb5204dc3b63c9850fdb410b214b0833211de
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-06-24 11:55:11 +0700

    fixed next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,20 +1,15 @@
-import type { NextConfig } from 'next';
-import path from 'path';
+import type { NextConfig } from "next";
+import path from "path";
 
-// Nếu đang chạy trên máy chủ Cloudflare (CF_PAGES = 1), dùng thẳng đường dẫn tuyệt đối của họ.
-// Nếu đang chạy dưới máy tính local của bạn, dùng process.cwd() để lùi ra 2 cấp an toàn.
 const workspaceRoot = process.env.CF_PAGES
-  ? '/opt/buildhome/repo'
-  : path.resolve(process.cwd(), '../../');
+  ? "/opt/buildhome/repo"
+  : path.resolve(process.cwd(), "../../");
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: workspaceRoot,
 
-  experimental: {
-    // @ts-expect-error: Bỏ qua lỗi Type do Next.js chưa cập nhật kịp thư viện Types cho turbopack
-    turbopack: {
-      root: workspaceRoot,
-    },
+  turbopack: {
+    root: workspaceRoot,
   },
 
   transpilePackages: ["@repo/ui"],
